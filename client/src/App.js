5c7c72506753c7a0ec52b9c0a4dd1dbe9c676719
Ivon <ivonne@jjf.com>
2022-01-23 17:52:12 -0600

    add sigup with errors

--- client/src/App.js
+++ client/src/App.js
@@ -1,26 +1,55 @@
 import React from 'react';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import { ApolloProvider, ApolloClient, InMemoryCache, createHttpLink } from '@apollo/client';
+import { setContext } from '@apollo/client/link/context';
+
 import './App.css';
 
 import NavBar from './components/NavBar';
 import Footer from './components/Footer'
-import Home from './pages/Home';
+import Home   from './pages/Home';
 import NoMatch from './pages/NoMatch';
+import Signup from './pages/Signup';
+import Login from './pages/Login';
+
+const httpLink = createHttpLink({
+  uri: '/graphql',
+});
+
+const authLink = setContext((_, { headers }) => {
+  const token = localStorage.getItem('id_token');
+  return {
+    headers: {
+      ...headers,
+      authorization: token ? `Bearer ${token}` : '',
+    },
+  };
+});
+
+const client = new ApolloClient({
+  link: authLink.concat(httpLink),
+  cache: new InMemoryCache(),
+});
+
 
 function App() {
   return (
+    <ApolloProvider client={client}>
       <Router>
         <div className="flex-column justify-flex-start min-100-vh">
         <NavBar/>
           <div className="container">
             <Switch>
                 <Route exact path="/" component={Home} />
+                <Route exact path="/login" component={Login} />
+                <Route exact path="/signup" component={Signup} />
                 <Route component={NoMatch} />
             </Switch>
           </div>
           <Footer />
         </div>
       </Router>
+   </ApolloProvider>
   );
 }
 export default App;
