52f6f95a3a68a16e4ed2e9d86451078c57e3d403
Ivon <ivonne@jjf.com>
2022-01-23 15:49:38 -0600

    add s3 bucket- display image

--- client/src/App.js
+++ client/src/App.js
@@ -1,25 +1,26 @@
-import logo from './logo.svg';
+import React from 'react';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import './App.css';
+
+import NavBar from './components/NavBar';
+import Footer from './components/Footer'
+import Home from './pages/Home';
+import NoMatch from './pages/NoMatch';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      <Router>
+        <div className="flex-column justify-flex-start min-100-vh">
+        <NavBar/>
+          <div className="container">
+            <Switch>
+                <Route exact path="/" component={Home} />
+                <Route component={NoMatch} />
+            </Switch>
+          </div>
+          <Footer />
+        </div>
+      </Router>
   );
 }
-
 export default App;
---
+++ client/src/utils/s3client.js
@@ -0,0 +1,14 @@
+//Purpose: s3Client.js is a helper function that creates an Amazon Simple Storage Service (Amazon S3) service client.
+// Create service client module using ES6 syntax.
+import { S3Client } from "@aws-sdk/client-s3";
+// Set the AWS Region.
+const region = "us-east-1"; //e.g. "us-east-1"
+// Create an Amazon S3 service client object.
+const s3Client = new S3Client({
+    region,
+    credentials: {
+        secretAccessKey: "ZGkVO0+zwub4B8eSPKwCqMDgaQsWiO8wu6t9Xejn",
+        accessKeyId: "AKIAU5YJF4CAE3ESRKHV"
+    }
+    })
+export { s3Client };
