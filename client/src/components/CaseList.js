9785edd905c381afafa4fa722f41bfe0edbe59d3
Cabral Williams <cabral.williams@gmail.com>
2022-02-03 12:25:42 -0600

    Minor edit to CaseList to address grammatical issue

--- client/src/components/CaseList.js
+++ client/src/components/CaseList.js
@@ -75,7 +75,7 @@ const CaseList = () => {
 			<div className="clearfix">
 			<div className="row width-80">
           		<div className="col-md-3 my-3 animated fadeIn text-center rounded "></div>
-				<div className="col-md-6 my-3 animated fadeIn text-center rounded section-heading h3 ">Someone, Somewhere is Looking for Them! Let's Help!!</div>
+				<div className="col-md-6 my-3 animated fadeIn text-center rounded section-heading h3 ">Someone, Somewhere, is Looking for Them! Let's Help!!</div>
             	<div className="col-md-6 my-3 animated fadeIn text-center rounded "></div>
 			</div>
 				<div className="row width-80">
